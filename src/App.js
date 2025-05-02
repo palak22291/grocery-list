dd8dab3eb80bb6cb869ee7c3de6303bb667973e1
Palak Gupta <palakgupta2208@gmail.com>
2025-05-03 01:27:28 +0530

    added image to herosection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import HeroSection  from "./components/HeroSection";
+import FeaturedItems from './components/FeaturedItems';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+
 
 
 function App() {
@@ -7,7 +12,10 @@ function App() {
     
        <>
        <Navbar/>
-
+       <HeroSection/>
+       <FeaturedItems/>
+       <HowItWorks/>
+        <Footer/>
        </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// components/Footer.js
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="#">📸</a>
+      <a href="#">📘</a>
+      <a href="#">🐦</a>
+    </footer>
+  );
+};
+
+export default Footer;
