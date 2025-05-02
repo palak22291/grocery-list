107b0c3ded1171e971141146bb646f8f189ce3fb
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-02 19:00:06 +0530

    made navbar componnet

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+function Navbar(){
+
+    return(
+        <nav>
+        <div>
+        Grocery Buddy
+        </div>
+
+        <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Recipes</li>
+        <li>Contact</li>
+        </ul>
+        </nav>
+    )
+
+}
+
+
+export default Navbar;
