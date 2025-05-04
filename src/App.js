09e4ee998544f6737236297240f80c72feec47c3
Palak Gupta <palakgupta2208@gmail.com>
2025-05-04 17:10:59 +0530

    added routing to navbar and made design changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import HeroSection  from "./components/HeroSection";
+import HeroSection from './components/HeroSection';
 import FeaturedItems from './components/FeaturedItems';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-
 function App() {
   return (
-    
-       <>
-       <Navbar/>
-       <HeroSection/>
-       <FeaturedItems/>
-       <HowItWorks/>
-        <Footer/>
-       </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/about" element={<HowItWorks />} />
+        <Route path="/recipes" element={<FeaturedItems />} />
+        <Route path="/contact" element={<Footer />} />
+      </Routes>
+    </Router>
   );
 }
 
