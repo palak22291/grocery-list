ba5123f2877d35edcc783fa5c5341644ec4b87aa
Palak Gupta <palakgupta2208@gmail.com>
2025-05-05 12:51:01 +0530

    modified js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,22 +5,22 @@ import groceryImg from '../assests/grocery-bag.png';
 
 
 
-function HeroSection(){
-    return(
-        <section className="hero">
-            <div className="hero-text">
-                <h1>Plan Your Grocery Like a Pro!</h1>
-                <p>Organize.Shop.Cook.Enjoy</p>
-                <button>Get Started</button>
-            </div>
-
-           
-            <img src={groceryImg}alt="Grocery Bag" className="hero-image" />
-
-         
-
-        </section>
-    )
-}
+function HeroSection() {
+    return (
+      <section className="hero">
+        <div className="hero-text">
+          <h1>
+            Plan Your 
+            Grocery 
+            Like a Pro!</h1>
+          <p>Organize. Shop. Cook. Enjoy</p>
+          <button>Get Started</button>
+        </div>
+  
+        <img src={groceryImg} alt="Grocery Bag" className="hero-image" />
+      </section>
+    );
+  }
+  
 
 export default HeroSection;
