5920a0c0183ad88dc9e96afdaca6b96f6ee841b4
Palak Gupta <palakgupta2208@gmail.com>
2025-05-15 23:58:09 +0530

    dummy urls added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,10 +15,19 @@ const Footer = () => {
         <div className="footer-section">
           <h4>Follow Us</h4>
           <div className="social-icons">
-            <a href="#"><i className="fab fa-facebook-f"></i></a>
+            {/* <a href="#"><i className="fab fa-facebook-f"></i></a>
             <a href="#"><i className="fab fa-twitter"></i></a>
             <a href="#"><i className="fab fa-linkedin-in"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a> */}
+
+            {/* using valid dummy urls  */}
+<a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+<a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+<a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+<a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+
+
+            
           </div>
         </div>
       </div>
