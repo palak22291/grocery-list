6a6e75ab3000bd1d8b794ff966d759ae9dcf8a5e
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-15 23:59:17 +0530

    made home component

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import FeaturedItems from "./FeaturedItems"
+import Footer from "./Footer"
+import HeroSection from "./HeroSection"
+import HowItWorks from "./HowItWorks"
+
+function Home(){
+    return(
+        <>
+        
+     
+        <HeroSection/>
+        <FeaturedItems/>
+        <HowItWorks/>
+        <Footer/>
+
+        </>
+    )
+}
+
+
+export default Home;
