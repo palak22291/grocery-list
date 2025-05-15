5c9523492701b9f0d0744673067a16c0667ab7d3
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-15 23:58:44 +0530

    did some changes in routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">
+        <Link to="/home">
           <img src="/Images/logo.png" alt="GroceryBuddy Logo" className="logo-img" />
         </Link>
       </div>
@@ -14,7 +14,7 @@ const Navbar = () => {
       <label htmlFor="menu-toggle" className="hamburger">&#9776;</label>
 
       <ul className="navbar-links">
-        <li className="home">Home</li>
+        <li><Link to="/home" className="home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/recipes">Recipes</Link></li>
         <li><Link to="/contact">Contact</Link></li>
