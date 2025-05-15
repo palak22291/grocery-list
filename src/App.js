b0d2f3646b80868970a26b33eb20e908a81f92bd
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-15 23:57:15 +0530

    made some routing changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
+// import './App.css';
+// import Navbar from './components/Navbar';
+// import HeroSection from './components/HeroSection';
+// import FeaturedItems from './components/FeaturedItems';
+// import HowItWorks from './components/HowItWorks';
+// import Footer from './components/Footer';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import SearchBar from './components/SearchBar';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <SearchBar/>
+//       <Home/>
+//       <Routes>
+//         <Route path="/" element={<HeroSection />} />
+//         <Route path="/about" element={<HowItWorks />} />
+//         <Route path="/recipes" element={<FeaturedItems />} />
+//         <Route path="/contact" element={<Footer />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
 import './App.css';
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
+import SearchBar from './components/SearchBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
 import FeaturedItems from './components/FeaturedItems';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <SearchBar/>
+      <SearchBar />
       <Routes>
-        <Route path="/" element={<HeroSection />} />
-        <Route path="/about" element={<HowItWorks />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/recipes" element={<FeaturedItems />} />
+        <Route path="/about" element={<HowItWorks />} />
         <Route path="/contact" element={<Footer />} />
       </Routes>
     </Router>
@@ -23,3 +53,5 @@ function App() {
 }
 
 export default App;
+
+
