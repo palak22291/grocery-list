36b5c0a12ac79ff1cd4f5269758b3bfae7ca56b7
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-22 20:03:50 +0530

    removed search bar from every component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,18 @@ import FeaturedItems from "./FeaturedItems"
 import Footer from "./Footer"
 import HeroSection from "./HeroSection"
 import HowItWorks from "./HowItWorks"
+import SearchBar from "./SearchBar"
 
 function Home(){
     return(
         <>
         
-     
+     <SearchBar/>
         <HeroSection/>
         <FeaturedItems/>
         <HowItWorks/>
         <Footer/>
+     
 
         </>
     )
