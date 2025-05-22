72837c1798c33b49e0a66bbea2aef58180f74c2f
Palak Gupta <palakgupta@Palaks-MacBook-Pro-2.local>
2025-05-22 20:02:02 +0530

    rendered signup and put routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-// import './App.css';
-// import Navbar from './components/Navbar';
-// import HeroSection from './components/HeroSection';
-// import FeaturedItems from './components/FeaturedItems';
-// import HowItWorks from './components/HowItWorks';
-// import Footer from './components/Footer';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import SearchBar from './components/SearchBar';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <SearchBar/>
-//       <Home/>
-//       <Routes>
-//         <Route path="/" element={<HeroSection />} />
-//         <Route path="/about" element={<HowItWorks />} />
-//         <Route path="/recipes" element={<FeaturedItems />} />
-//         <Route path="/contact" element={<Footer />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 import './App.css';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 
 import Home from './components/Home';
 import FeaturedItems from './components/FeaturedItems';
@@ -41,12 +13,16 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <SearchBar />
+      
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/recipes" element={<FeaturedItems />} />
         <Route path="/about" element={<HowItWorks />} />
         <Route path="/contact" element={<Footer />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/searchbar" element={<SearchBar/>} />
+
+
       </Routes>
     </Router>
   );
