5e0f49603520380aed50c237cde1d675a4550e0a
Palak Gupta <palakgupta2208@gmail.com>
2025-05-25 13:32:11 +0530

    added google sign in

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,21 +1,20 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBf-jYmeblmHW-bFkLdiHj1lJKE7SSIXz0",
   authDomain: "grocerybuddy-6bdb6.firebaseapp.com",
   projectId: "grocerybuddy-6bdb6",
-  storageBucket: "grocerybuddy-6bdb6.appspot.com", // ✅ Fixed this
+  storageBucket: "grocerybuddy-6bdb6.appspot.com", 
   messagingSenderId: "1047437779574",
   appId: "1:1047437779574:web:79d87cfbaca0b7a1c1f557",
   measurementId: "G-TS3VE4RPHC"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// ✅ Add auth initialization
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export { auth };
+export { auth, provider };
+
