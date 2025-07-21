40818983dcbd21f1fc7a1ea472a8ab41c441b70f
Palak Gupta <palakgupta2208@gmail.com>
2025-07-22 04:37:11 +0530

    renamed

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Firebase config file
+import { initializeApp } from "firebase/app";
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBf-jYmeblmHW-bFkLdiHj1lJKE7SSIXz0",
+  authDomain: "grocerybuddy-6bdb6.firebaseapp.com",
+  projectId: "grocerybuddy-6bdb6",
+  storageBucket: "grocerybuddy-6bdb6.appspot.com", 
+  messagingSenderId: "1047437779574",
+  appId: "1:1047437779574:web:79d87cfbaca0b7a1c1f557",
+  measurementId: "G-TS3VE4RPHC"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const db = getFirestore(app);
+
+export { auth, provider ,db};
+
