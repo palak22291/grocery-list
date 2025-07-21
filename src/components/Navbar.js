826f491f696d4577981b05245585f66ed83e71e8
Palak Gupta <palakgupta2208@gmail.com>
2025-07-22 04:10:11 +0530

    fixed css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,46 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/home">
-          <img src="/Images/logo.png" alt="GroceryBuddy Logo" className="logo-img" />
+          <img
+            src="/Images/logo.png"
+            alt="GroceryBuddy Logo"
+            className="logo-img"
+          />
         </Link>
       </div>
 
       <input type="checkbox" id="menu-toggle" className="menu-toggle" />
-      <label htmlFor="menu-toggle" className="hamburger">&#9776;</label>
+      <label htmlFor="menu-toggle" className="hamburger">
+        &#9776;
+      </label>
 
       <ul className="navbar-links">
-        <li><Link to="/home" className="home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/grocery">Grocery</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        {/* <li><Link to="/login" className="login-btn">Login</Link></li> */}
-        <li><Link to="/signup" className="login-signup-btn">Login / Sign up</Link></li>
+        <li>
+          <Link to="/home" className="home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/grocery">Grocery</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+
+        <li>
+          <Link to="/signup" className="login-signup-btn">
+            Login / Sign up
+          </Link>
+        </li>
       </ul>
     </nav>
   );
